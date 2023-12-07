386d6b52bcade38e108efdffd5b3ee0106203267
Benjamin Siegmund <rialgar42@gmail.com>
2023-12-07 09:10:08 +0100

    day 7

---
+++ 7/7.2.mjs
@@ -0,0 +1,63 @@
+import { initArray, readLines } from "../utils.mjs";
+
+const HAND_SIZE = 5;
+const CARDS = 'J23456789TQKA';
+const NUM_CARDS = CARDS.length;
+const WEIGHTS = [];
+for(let i = 0; i <= HAND_SIZE; i++){
+    WEIGHTS[i] = Math.pow(NUM_CARDS, i);
+}
+
+const lines = await readLines('input');
+
+function rateHand(hand) {
+    let rating = 0;
+    if(hand.length != HAND_SIZE){
+        throw new Error('invalid hand: ', hand);
+    }
+    const counters = initArray(NUM_CARDS, 0);
+
+    for(let i = 0; i < HAND_SIZE; i++){
+        const card = hand[hand.length-i-1];
+        const cardValue = CARDS.indexOf(card);
+        if(cardValue < 0){
+            throw new Error('invalid card: ', card, 'in hand: ', hand);
+        }
+        counters[cardValue] += 1;
+        rating += cardValue * WEIGHTS[i];
+    }
+
+    const jokers = counters[0];
+    const maxCounters = counters.slice(1).filter(c => c>0).sort((a,b) => b-a);
+    if(maxCounters[0] + jokers === 5 || jokers === 5){
+        rating += 6 * WEIGHTS[HAND_SIZE];
+    } else if(maxCounters[0] + jokers === 4){
+        rating += 5 * WEIGHTS[HAND_SIZE];
+    } else if(maxCounters[0] + jokers === 3 && maxCounters[1] === 2){
+        rating += 4 * WEIGHTS[HAND_SIZE];
+    } else if(maxCounters[0] + jokers === 3){
+        rating += 3 * WEIGHTS[HAND_SIZE];
+    } else if(maxCounters[0] + jokers === 2 && maxCounters[1] === 2){
+        rating += 2 * WEIGHTS[HAND_SIZE];
+    } else if(maxCounters[0] + jokers === 2){
+        rating += 1 * WEIGHTS[HAND_SIZE];
+    }
+    return rating;
+}
+
+const handsAndBids = lines
+    .map(line => line.split(' '))
+    .map(([hand, bid]) => ({
+        hand,
+        bid:parseInt(bid),
+        rating: rateHand(hand)
+    }))
+    .sort(({rating: a}, {rating: b}) => a-b);
+
+console.log(handsAndBids);
+let sum = 0;
+for(let i = 0; i<handsAndBids.length; i++){
+    const rank = i+1;
+    sum += rank * handsAndBids[i].bid;
+}
+console.log(sum);
--- utils.mjs
+++ utils.mjs
@@ -1,6 +1,15 @@
 import fs from 'node:fs/promises';
+import { isInt8Array } from 'node:util/types';
 
 export async function readLines(file){
     const contents = await fs.readFile(file, {encoding: 'utf8'});
     return contents.split('\n');
+}
+
+export function initArray(size, value){
+    const arr = new Array(size);
+    for(let i = 0; i <= size; i++){
+        arr[i] = value;
+    }
+    return arr;
 }
